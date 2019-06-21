17bcfdc210f995f89ba61acb134686b445f38efd
张发亮 <fl.zhang@8win.com>
2019-06-21 14:32:08 +0800

    最新解析

--- server/servercp.js
+++ server/servercp.js
@@ -6,37 +6,36 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const proxy = require("koa-proxies");
 const { get } = require("lodash");
-const fs = require('fs')
-const reptileUrl ="https://m.weibo.cn/api/container/getIndex?containerid=102803_ctg1_1988_-_ctg1_1988&openApp=0"
-// const reptileUrl ="https://m.weibo.cn/api/container/getIndex?containerid=102803_ctg1_4388_-_ctg1_4388&openApp=0";
-const prot = 9000;
+const fs = require("fs");
+// const reptileUrl ="https://m.weibo.cn/api/container/getIndex?containerid=102803_ctg1_1988_-_ctg1_1988&openApp=0"
+const reptileUrl =
+  "https://m.weibo.cn/api/container/getIndex?containerid=102803_ctg1_4388_-_ctg1_4388&openApp=0";
+const prot = 5000;
 
-const apiPromise = (since_id) => {
+const apiPromise = since_id => {
   return new Promise((res, rej) => {
     axios
       .get(reptileUrl)
-      .then(function (response) {
+      .then(function(response) {
         res(response);
       })
-      .catch(function (error) {
+      .catch(function(error) {
         rej(error);
       })
-      .then(function () {
+      .then(function() {
         // always executed
       });
   });
 };
 
-
 app.use(async ctx => {
+  const datas = [];
+  datas.push(await apiPromise(1));
 
-  const datas = []
-  datas.push(await apiPromise(1))
-
-  const processData = datas.map((data) => {
+  const processData = datas.map(data => {
     return get(data, "data.data.cards", []).map(item => {
       const mblog = get(item, "mblog", {});
-      const { thumbnail_pic, bmiddle_pic, original_pic, text } = mblog;
+      const { thumbnail_pic, bmiddle_pic, original_pic, text, pics } = mblog;
       let texts = [],
         reg = /[^A-z|0-9|=|>|<|\-|\；|\:|\"|\\|/|.|;|\? |+, +|,|\?|\.|\&|\%]+/g;
       let res = true;
@@ -49,20 +48,31 @@ app.use(async ctx => {
         thumbnail_pic,
         bmiddle_pic,
         original_pic,
-        title: texts.join(',')
+        title: texts.join(","),
+        pics
       };
     });
-
-  })
-
-  fs.writeFile('app.json', JSON.stringify(processData).replace(/\[|\]/img, '').replace(/\},/img, '}'), 'utf-8', (err) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log('文件已被保存');
+  });
+  //false}}}]} 
+  fs.writeFile(
+    "app.json",
+    JSON.stringify(processData)
+      .replace(/\[|\]/gim, "")
+      .replace(/\"pics\":/gim, '"pics":[')
+      .replace(/false\}\}\}\}/gim, 'false}}}]}')
+      .replace(/true\}\}\}\}/gim, 'true}}}]}')
+      .replace(/\},/gim, "}")
+      .replace(/\}\"large\"/gim, '},"large"')
+      .replace(/\}\}\}\{\"pid\"/gim, '}}},{"pid"'),
+    "utf-8",
+    err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("文件已被保存");
+      }
     }
-
-  })
+  );
   ctx.body = {
     url: processData
   };
