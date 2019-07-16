83892d922cf3b22a8a6251182b3d269a8fbdabfc
张发亮 <fl.zhang@8win.com>
2019-07-16 16:11:23 +0800

    add吃鸡

---
+++ server/writeFile/advertising.js
@@ -0,0 +1,30 @@
+const {
+    get
+} = require("lodash");
+const {
+    processDatas
+} = require("./adcertsingData.js");
+
+const fs = require("fs");
+
+fs.writeFile(
+    "advertising.json",
+    JSON.stringify(processDatas)
+    // .replace(/\[|\]/gim, "")
+    .replace(/\"pics\":/gim, '"pics":[')
+    .replace(/false\}\}\}\}/gim, 'false}}}]}')
+    .replace(/true\}\}\}\}/gim, 'true}}}]}')
+    // .replace(/\},/gim, "}")
+    .replace(/\}\"large\"/gim, '},"large"')
+    .replace(/\}\}\}\{\"pid\"/gim, '}}},{"pid"')
+    .replace(/null,/gim, '')
+    .replace(/',|#,/gim, ""),
+    "utf-8",
+    err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("文件已被保存");
+        }
+    }
+);
